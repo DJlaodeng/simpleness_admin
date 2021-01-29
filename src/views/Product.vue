be3aa927caf5c93cf3385d4d50255eec3bf03c3f
DJ老邓 <panda19950826@qq.com>
2021-01-29 17:32:05 +0800

    商品管理表格数据渲染

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -19,11 +19,44 @@
     </el-form>
     <!-- 表格数据 -->
     <el-table :data="tableData" border stripe style="width: 100%">
-      <el-table-column prop="date" label="ID" width="150"> </el-table-column>
-      <el-table-column prop="name" label="信息"> </el-table-column>
-      <el-table-column prop="address" label="价格"> </el-table-column>
-      <el-table-column prop="address" label="状态"> </el-table-column>
-      <el-table-column prop="address" label="操作"> </el-table-column>
+      <el-table-column prop="id" label="ID" width="150"> </el-table-column>
+      <el-table-column prop="name" label="信息">
+        <template slot-scope="scope">
+          <p>{{ scope.row.name }}</p>
+          <p>{{ scope.row.subtitle }}</p>
+        </template>
+      </el-table-column>
+      <el-table-column prop="price" label="价格"> </el-table-column>
+      <el-table-column prop="status" label="状态">
+        <template slot-scope="scope">
+          <span v-if="scope.row.status == 1">在售</span>
+          <span v-if="scope.row.status == 2">已下架</span>
+          <el-button
+            class="mini"
+            size="mini"
+            type="warning"
+            v-if="scope.row.status == 1"
+            >下架</el-button
+          >
+          <el-button
+            class="mini"
+            size="mini"
+            type="warning"
+            v-if="scope.row.status == 2"
+            >上架</el-button
+          >
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button @click="lookClick(scope.row)" type="text" size="small"
+            >查看</el-button
+          >
+          <el-button @click="editClick(scope.row)" type="text" size="small"
+            >编辑</el-button
+          >
+        </template>
+      </el-table-column>
     </el-table>
     <!-- 分页器 -->
   </div>
@@ -52,16 +85,25 @@ export default {
     async getTableData() {
       let { data: res } = await this.$axios.product({ pageNum: this.pageNum });
       console.log(res);
+      this.tableData = res.data.list;
     },
     // 查询
     onInquire() {
       console.log("submit!");
-    }
+    },
+    // 查看
+    lookClick() {},
+    // 编辑
+    editClick() {}
   }
 };
 </script>
 
 <style lang="scss" scoped>
-// .product {
-// }
+.product {
+  .mini {
+    padding: 5px;
+    margin-left: 3px;
+  }
+}
 </style>
